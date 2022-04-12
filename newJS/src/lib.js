91e50b401748cc30c18c7cbf3ffe1bdd88aaf9d7
kritikmodi <kritikmodi@gmail.com>
2022-04-12 19:35:50 +0530

    Add JS runnables

--- newJS/src/lib.js
+++ newJS/src/lib.js
@@ -1,9 +1,25 @@
 import { log, http } from "@suborbital/runnable";
+import { db } from "@suborbital/runnable"
+import { cache } from "@suborbital/runnable"
 
-export const run = (input) => {
-  const response = http.get("https://catfact.ninja/fact");
+export const run = (variable) => {
+  const response = http.get("https://pokeapi.co/api/v2/pokemon/"+variable);
+
+  const responsePOST = http.post("https://jsonplaceholder.typicode.com/posts", JSON.stringify({
+    id: 101,
+    title: 'foo',
+    body: 'bar',
+    userId: 1
+  }
+  ));
+
+  db.insert("Pokemon1");
+
   log.info(response.text());
   const obj = response.json();
   const val = JSON.parse(JSON.stringify(obj));
-  return (val.fact);
+
+  cache.set("Pokemon", val.weight);
+  
+  return (val.weight);
 };
--- pokeweight/src/lib.js
+++ pokeweight/src/lib.js
@@ -1,9 +1,14 @@
 import { log } from "@suborbital/runnable";
 import { http } from "@suborbital/runnable"
+import { cache } from "@suborbital/runnable"
 
 export const run = (input) => {
   const response = http.get("https://catfact.ninja/fact");
+
   log.info(response.text());
-  const obj = JSON.parse('{"fact":"Cat families usually play best in even numbers. Cats and kittens should be acquired in pairs whenever possible.","length":111}');
-  return obj.fact;
+
+  //const obj = JSON.parse('{"fact":"Cat families usually play best in even numbers. Cats and kittens should be acquired in pairs whenever possible.","length":111}');
+  
+  const weight = cache.get("Pokemon");
+  return weight;
 };
